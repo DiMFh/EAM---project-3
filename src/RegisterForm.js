73d5247f5530e5a127361d0b7db790050cbade93
DiMFh <d_fotopoulos80@yahloo.com>
2024-01-15 08:23:24 +0200

    Register changes + courses/db changes

--- src/RegisterForm.js
+++ src/RegisterForm.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
-import { doc, setDoc } from 'firebase/firestore'
+import { doc, setDoc,getDoc } from 'firebase/firestore'
+import { Form, Button,  Container, Row, Col, Alert } from 'react-bootstrap';
 import { courses } from './Utils/Objects/objects';
-import './RegisterForm.css'
+//import './RegisterForm.css'
 
 export default function RegisterForm({db}){
 
@@ -52,7 +53,14 @@ export default function RegisterForm({db}){
       // At the same time we push all the courses at the db.
       // We create a 'courses' collection with primary key 'all_courses'
       const ref_courses = doc(db, "courses", "all_courses");
-      await setDoc(ref_courses, courses);
+  
+        // Έλεγχος αν το έγγραφο μαθημάτων υπάρχει
+        const docSnap = await getDoc(ref_courses);
+
+        // Αν το έγγραφο δεν υπάρχει, τότε προσθήκη του
+        if (!docSnap.exists()) {
+            await setDoc(ref_courses, courses);
+        }
 
       // Redirect to login route
       window.location.href = '/login';
@@ -64,82 +72,86 @@ export default function RegisterForm({db}){
     }
     
   }
-    return(
-      <div className='register'>
-        <div className='info-div'>
-          <p style={{ fontWeight: 'bold' }}>Ότι έχει * είναι υποχρεωτικό να συμπληρωθεί.</p>
-          <p>Παρακαλώ συμπληρώστε τα στοιχεία σας.</p>
-        </div>
-        <form onSubmit={handleRegister} className='register-container'>
-           
-            <div className='register-row'>
-                <label>Ηλεκτρονική Διεύθυνση*</label>
-                &nbsp;&nbsp;&nbsp;
-                <input
-                    type="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                />
-            </div>
-            <div className='register-row'>
-                <label>Κωδικός*</label>
-                &nbsp;&nbsp;&nbsp;
-                <input
-                    type="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-            </div>
-            <div className='register-row'>
-                <label>Επιβεβαίωση Κωδικού*</label>
-                &nbsp;&nbsp;&nbsp;
-                <input
-                    type="password"
-                    value={conpassword}
-                    onChange={(e) => setConPassword(e.target.value)}
-                />
-            </div>
-            <div className='register-row'>
-                <label>Ημερομηνία Γέννησης*</label>
-                &nbsp;&nbsp;&nbsp;
-                <input
-                    type="date"
-                    value={date}
-                    onChange={(e) => setDate(e.target.value)}
-                />
-            </div>
-            <div className='register-row'>
-              <label>Ρόλος*</label>
-              &nbsp;&nbsp;&nbsp;
-              <select
-                  value={role}
-                  onChange={(e) => setRole(e.target.value)}
-              >
-                  <option value="professor">Professor</option>
-                  <option value="student">Student</option>
-              </select>
-            </div>
-            <div className='register-row'>
-                <label>Τηλέφωνο*</label>
-                &nbsp;&nbsp;&nbsp;
-                <input
-                    type="telephone number"
-                    value={telephone}
-                    onChange={(e) => setTelephone(e.target.value)}
-                />
-            </div>
-            <div className='register-row'>
-                <label>Διεύθυνση</label>
-                &nbsp;&nbsp;&nbsp;
-                <input
-                    type="address"
-                    value={address}
-                    onChange={(e) => setAddress(e.target.value)}
-                />
-            </div>
-            <button type='submit'>Register</button>
-            <a href='/login'>Already have an Account?</a>
-        </form>
-      </div>
-    )
+    
+
+
+  return (
+    <Container style={{ backgroundColor: '#123456', color: '#a9a9a9' }}>
+        <Alert variant='info'>
+            <p style={{ fontWeight: 'bold' }}>Ότι έχει * είναι υποχρεωτικό να συμπληρωθεί.</p>
+            <p>Παρακαλώ συμπληρώστε τα στοιχεία σας.</p>
+        </Alert>
+        <Form onSubmit={handleRegister} >
+            <Form.Group as={Row} className='mb-3' controlId="formPlaintextEmail">
+                <Form.Label column sm="2">
+                    Ηλεκτρονική Διεύθυνση*
+                </Form.Label>
+                <Col sm="3">
+                    <Form.Control type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                </Col>
+            </Form.Group>
+            <Form.Group as={Row} className='mb-3' controlId="formPlaintextPassword">
+      <Form.Label column sm="2">
+          Κωδικός*
+      </Form.Label>
+      <Col sm="3">
+          <Form.Control type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      </Col>
+  </Form.Group>
+
+  <Form.Group as={Row} className='mb-3' controlId="formPlaintextConfirmPassword">
+      <Form.Label column sm="2">
+          Επιβεβαίωση Κωδικού*
+      </Form.Label>
+      <Col sm="3">
+          <Form.Control type="password" placeholder="Confirm Password" value={conpassword} onChange={(e) => setConPassword(e.target.value)} />
+      </Col>
+  </Form.Group>
+
+  <Form.Group as={Row} className='mb-3' controlId="formPlaintextDate">
+      <Form.Label column sm="2">
+          Ημερομηνία Γέννησης*
+      </Form.Label>
+      <Col sm="3">
+          <Form.Control type="date" value={date} onChange={(e) => setDate(e.target.value)} />
+      </Col>
+  </Form.Group>
+
+  <Form.Group as={Row} className='mb-3' controlId="formPlaintextRole">
+      <Form.Label column sm="2">
+          Ρόλος*
+      </Form.Label>
+      <Col sm="3">
+          <Form.Control as="select" value={role} onChange={(e) => setRole(e.target.value)}>
+              <option value="professor">Professor</option>
+              <option value="student">Student</option>
+          </Form.Control>
+      </Col>
+  </Form.Group>
+
+  <Form.Group as={Row} className='mb-3' controlId="formPlaintextPhone">
+      <Form.Label column sm="2">
+          Τηλέφωνο*
+      </Form.Label>
+      <Col sm="3">
+          <Form.Control type="tel" placeholder="Telephone Number" value={telephone} onChange={(e) => setTelephone(e.target.value)} />
+      </Col>
+  </Form.Group>
+
+  <Form.Group as={Row} className='mb-3' controlId="formPlaintextAddress">
+      <Form.Label column sm="2">
+          Διεύθυνση
+      </Form.Label>
+      <Col sm="3">
+          <Form.Control type="text" placeholder="Address" value={address} onChange={(e) => setAddress(e.target.value)} />
+      </Col>
+  </Form.Group>
+
+  <Button variant="primary" type="submit">
+      Register
+  </Button>
+  <a href='/login' className='mt-3 d-block'>Already have an Account?</a>
+  </Form>
+  </Container>
+  );
 }
