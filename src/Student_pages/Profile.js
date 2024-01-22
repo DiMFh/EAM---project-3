3f3d1168ec7b258079e16a2f172f54d29898b272
DiMFh <d_fotopoulos80@yahoo.com>
2024-01-22 17:13:38 +0200

    Profile

--- src/Student_pages/Profile.js
+++ src/Student_pages/Profile.js
@@ -2,7 +2,7 @@
 
 
 import React, { useState, useEffect } from 'react';
-import { doc, getDoc } from 'firebase/firestore';
+import { doc, getDoc,updateDoc  } from 'firebase/firestore';
 import {
   MDBCol,
   MDBContainer,
@@ -12,18 +12,14 @@ import {
   MDBCardBody,
   MDBCardImage,
   MDBBtn,
-  MDBBreadcrumb,
-  MDBBreadcrumbItem,
-  MDBProgress,
-  MDBProgressBar,
-  MDBIcon,
-  MDBListGroup,
-  MDBListGroupItem
 } from 'mdb-react-ui-kit';
+import { Breadcrumb } from 'react-bootstrap';
 import './Profile.css';
 
 const ProfilePage = ({ db }) => {
   const [userData, setUserData] = useState(null);
+  const [editMode, setEditMode] = useState(false);
+  const [newData, setNewData] = useState({});
   const userEmail = localStorage.getItem('email');
 
     useEffect(() => {
@@ -47,8 +43,45 @@ const ProfilePage = ({ db }) => {
         fetchUserData();
     }, [db, userEmail]);
 
+    const handleImageChange = (e) => {
+      
+      if (e.target.files && e.target.files[0]) {
+        let selectedFile = e.target.files[0];
+        let reader = new FileReader();
+        reader.onload = (e) => {
+          
+          setNewData({ ...newData, avatar: e.target.result });
+        };
+        reader.readAsDataURL(selectedFile);
+      }
+    };
+
+    const handleEdit = () => {
+      setEditMode(true);
+      setNewData({ ...userData }); // Αρχικοποιήστε τη φόρμα με τα υπάρχοντα δεδομένα
+    };
+
+    const handleSave = async () => {
+      try {
+        const docRef = doc(db, "users", userEmail);
+        await updateDoc(docRef, newData);
+        setUserData(newData);
+        setEditMode(false);
+      } catch (error) {
+        console.error("Error updating document: ", error);
+      }
+    };
+    const handleInputChange = (event) => {
+      const { name, value } = event.target;
+      setNewData({ ...newData, [name]: value });
+    };
+
     return (
       <section style={{ backgroundColor: '#eee' }}>
+      <Breadcrumb>
+        <Breadcrumb.Item href="./">Αρχική</Breadcrumb.Item>
+        <Breadcrumb.Item active>Προφίλ</Breadcrumb.Item>
+      </Breadcrumb>
         {userData && (
           <MDBContainer className="py-5">
             <MDBRow>
@@ -61,13 +94,24 @@ const ProfilePage = ({ db }) => {
                       className="rounded-circle"
                       style={{ width: '200px' }}
                       fluid />
-                    
+                      <div>
+                        <label htmlFor="imageUpload" className="btn btn-primary btn-sm mt-2">
+                          Επιλογή Εικόνας
+                        </label>
+                        <input
+                          type="file"
+                          id="imageUpload"
+                          style={{ display: 'none' }}
+                          onChange={handleImageChange} // Μέθοδος για την επεξεργασία της νέας εικόνας
+                        />
+                    </div>
                     <div className="d-flex justify-content-center mb-2">
                       <MDBCol lg="4">
-                        <p className="text-muted mb-1"><strong>Email: </strong> {userData.email}</p>
-                        <p className="text-muted mb-1"><strong>Ρόλος: </strong> {userData.role}</p>
+                        <p className="text-muted mb-1" style={{ textAlign: 'left' }}><strong>Email: </strong> {userData.email}</p>
+                        <p className="text-muted mb-1" style={{ textAlign: 'left' }}><strong>Ρόλος: </strong> {userData.role}</p>
                       </MDBCol>  
                     </div>
+                    
                   </MDBCardBody>
                 </MDBCard>
               </MDBCol>
@@ -122,22 +166,56 @@ const ProfilePage = ({ db }) => {
                   </MDBCardBody>
                 </MDBCard>
                 <MDBCard className="mb-2">
-                  <MDBCardBody>
-                    <MDBRow>
-                      <MDBCol sm="3">
-                        <MDBCardText>Πόλη: </MDBCardText>
-                        <MDBCardText>Διεύθυνση: </MDBCardText>
-                        <MDBCardText>Ταχυδρομικός Κώδικας: </MDBCardText>
+            <MDBCardBody>
+              <MDBRow>
+                <MDBCol sm="3">
+                  <MDBCardText>Πόλη: </MDBCardText>
+                  <MDBCardText>Διεύθυνση: </MDBCardText>
+                  <MDBCardText>Ταχυδρομικός Κώδικας: </MDBCardText>
+                </MDBCol>
+                <MDBCol sm="9" className="text-start">
+                  {editMode ? (
+                    <>
+                      <MDBCol sm="9" className="text-start">
+                        <input
+                          type="text"
+                          name="city"
+                          value={newData.city}
+                          onChange={handleInputChange}
+                        />
                       </MDBCol>
                       <MDBCol sm="9" className="text-start">
-                        <MDBCardText className="text-muted me-2">{userData.city}</MDBCardText>
-                        <MDBCardText className="text-muted">{userData.address}</MDBCardText>
-                        <MDBCardText className="text-muted">{userData.zip}</MDBCardText>
+                        <input
+                          type="text"
+                          name="address"
+                          value={newData.address}
+                          onChange={handleInputChange}
+                        />
                       </MDBCol>
+                      <MDBCol sm="9" className="text-start">
+                        <input
+                          type="text"
+                          name="zip"
+                          value={newData.zip}
+                          onChange={handleInputChange}
+                        />
+                      </MDBCol>
+                    </>
+                  ) : (
+                    <>
+                      <MDBCardText className="text-muted me-2">{userData.city}</MDBCardText>
+                      <MDBCardText className="text-muted">{userData.address}</MDBCardText>
+                      <MDBCardText className="text-muted">{userData.zip}</MDBCardText>
+                    </>
+                  )}
+                  {!editMode && <MDBBtn onClick={handleEdit} color="primary" className="btn-rounded" size="md">Επεξεργασία Στοιχείων</MDBBtn>}
+                  {editMode && <MDBBtn onClick={handleSave} color="success" className="btn-rounded" size="md">Αποθήκευση Αλλαγών</MDBBtn>}
+                </MDBCol>
+              </MDBRow>
+              </MDBCardBody>
+            </MDBCard>  
 
-                    </MDBRow>
-                  </MDBCardBody>
-                </MDBCard>
+            
               </MDBCol>   
             </MDBRow>  
           </MDBContainer>
