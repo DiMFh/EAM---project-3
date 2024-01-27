c08095b37d71718d06ddbeb1273988a9c0f99688
Angelos-Tsitsoli <tsitsoliaggelos@gmail.com>
2024-01-27 18:12:50 +0200

    Carousel update

--- src/Home.js
+++ src/Home.js
@@ -2,20 +2,25 @@
 import React from 'react';
 import headerimg from "./images/EKPA.jpg";
 import InfoCardsContainer from "./InfoCardsContainer";
+import Homecarousel from "./Homecarousel";
 import "./App.css";
+import "./Home.css";
 
 const Home = () => {
   return (
+    // <div>
+    //     {/* Edw vazoume olous tous components */}
+    //     <header className="header">
+    //       <h1>
+    //         {" "}
+    //         Γραμματίες Εθνικού και Καποδιστριακού <br></br> Πανεπιστημίου Αθηνών
+    //       </h1>
+    //       <img className="homepage-img" src={headerimg} alt="headerimg" />
+    //     </header>
+    //     <InfoCardsContainer />
+    // </div>
     <div>
-        {/* Edw vazoume olous tous components */}
-        <header className="header">
-          <h1>
-            {" "}
-            Γραμματίες Εθνικού και Καποδιστριακού <br></br> Πανεπιστημίου Αθηνών
-          </h1>
-          <img className="homepage-img" src={headerimg} alt="headerimg" />
-        </header>
-        <InfoCardsContainer />
+    <Homecarousel className="parent"/>
     </div>
   );
 };
